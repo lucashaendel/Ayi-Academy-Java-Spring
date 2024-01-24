4d02fe346cb1f6852b086f9d99b5d89f5b96586a
lucashaendel <lucas.ph@live.com>
2024-01-23 22:25:46 -0300

    update clase 12

--- clase12/ejerciciodos/src/main/java/com/clasedoce/ejerciciodos/config/SecurityConfig.java
+++ clase12/ejerciciodos/src/main/java/com/clasedoce/ejerciciodos/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.clasedoce.ejerciciodos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,13 +17,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain webSecurity(HttpSecurity http)throws Exception{
-//        return http.authorizeHttpRequests(auth->{
-//            auth.requestMatchers("/publico").permitAll();
-//                    auth.anyRequest().authenticated();
-//        }).build();
-//    }
+    @Bean
+    public SecurityFilterChain webSecurity(HttpSecurity http)throws Exception{
+
+         http.authorizeHttpRequests(auth->{
+            auth.requestMatchers("/publico").permitAll();
+                    auth.anyRequest().authenticated();
+        }
+         ).formLogin(Customizer.withDefaults())
+                 .httpBasic(Customizer.withDefaults());
+         return http.build();
+    }
 
     @Bean
     public UserDetailsService usuarioCreado(){
@@ -32,5 +39,17 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("Lucas").password(passwordEncoder().encode("1234")).roles().build());
+//        return manager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
